f0d76b38e98ad0808a7eedddc13e77680f1fedd5
Elaman Imashov <elaman@outlook.com>
2020-05-27 09:18:58 +0600

    Restructure project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from "react";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
 import Items from "./Items/Items";
 import classes from "./App.module.css";
-import reducer from "./reducers/items";
-
-// 2. Store
-const store = createStore(reducer);
 
 export default () => (
   <div className={classes.App}>
-    <Provider store={store}>
       <Items />
-    </Provider>
   </div>
 );
